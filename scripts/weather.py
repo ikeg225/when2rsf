19e4048ae970481a49e0a16fea8fda16f796aad1
Damond Fang <damondfang@MacBook-Air.local>
2023-09-18 19:48:38 -0700

    update

--- scripts/weather.py
+++ scripts/weather.py
@@ -6,3 +6,10 @@ load_dotenv()
 
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
+parameters = {
+    'q': 'berkeley'
+}
+
+response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}', params = parameters)
+
+print(response.text)
