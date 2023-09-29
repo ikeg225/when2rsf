81d2200c6a8871b8982c7b4b36990f9055dc8c59
Aiji Li <aijili@wifi-10-40-95-17.wifi.berkeley.edu>
2023-09-28 22:05:57 -0700

    def statement

--- scripts/weather.py
+++ scripts/weather.py
@@ -3,92 +3,102 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def get_dict():
+    load_dotenv()
 
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+    WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
-parameters = {
-    'q': 'berkeley',
-    'days': 3,
-    'exclude': 'current,minutely,hourly,astronomy'
-}
-
-
-response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}', params = parameters)
-
-#print(response.text)
-if response.status_code == 200:
-    # The request was successful (HTTP 200 OK).
-    
-    # Parse the JSON response
-    data = response.json()
-    
-    # Put data into dictionary
-    data_dict = {
-        'time' : data['current']['time'],
-        'day_of_the_week' : data['current']['is_day'] + 1,
-        'temperature' : data['current']['temp_f'],
-        #'description' : data['current']['condition']['text'],
-        'temp_feel' : data['current']['feelslike_f'],
-        'weather_code' : data['current']['code'],
-        'wind_mph' : data['current']['wind_mph'],
-        'wind_degree' : data['current']['wind_degree'],
-        'pressure_mb' : data['current']['pressure_mb'],
-        'precipitation_mm' : data['current']['precip_mm'],
-        'humidity' : data['current']['humidity'],
-        'cloudiness' : data['current']['cloud'],
-        'uv_index' : data['current']['uv'],
-        'gust_mph' : data['current']['gust_mph'],
+    parameters = {
+        'q': 'berkeley',
+        'days': 3,
+        'exclude': 'current,minutely,hourly,astronomy'
     }
-    
-    # Access and use the data
-    temperature = data['current']['temp_f']
-    description = data['current']['condition']['text']
-    day_of_the_week = data['current']['is_day'] + 1
-    temp_feel = data['current']['feelslike_f']
-    weather_code = data['current']['code']
-    # Get today's date and calculate the date for the next day
-    today = datetime.now().date()
-    next_day = today + timedelta(days=1)
-
-    # Find the forecast for the next day
-    for forecast in data['forecast']['forecastday']:
-        date = datetime.strptime(forecast['date'], '%Y-%m-%d').date()
-        if date == next_day:
-            next_day_forecast = forecast
-            break
-    
-    #chance_of_rain = tomorrow_forecast['day']['daily_chance_of_rain']
-    # Print the weather information
-    print("Date:", today)
-    print(f"Day of the Week: {day_of_the_week}")
-    print(f"Temperature: {temperature}°F")
-    print(f"Feels Like: ", temp_feel, "°F")
-    print(f"Weather Code: ", weather_code)
-    print(f"Description: {description}")
-    print()
-    # Print forcast for next day
-    print("Forecast for the next day:")
-    print(f"Date: {next_day_forecast['date']}")
-  #  print(f"Day of the Week: {next_day_forecast['is_day']}")
-    print(f"Temperature: {next_day_forecast['day']['avgtemp_f']}°F")
-    #print(f"Description: {next_day_forecast['day']['condition']['text']}") this prints text, not a percentage of probability it will rain
-    print(f"Description: {next_day_forecast['day']['daily_chance_of_rain']}") #this just adds all the percentages together
-    print("other alg")
-    tomorrow = today + timedelta(days=1)
-    for forecast in data['forecast']['forecastday']:
-        date = datetime.strptime(forecast['date'], '%Y-%m-%d').date()
-        if date == tomorrow:
-            tomorrow_forecast = forecast
-            break
-
-    # Extract the chance of rain for tomorrow
-    chance_of_rain = tomorrow_forecast['day']['daily_chance_of_rain']
-
-    # Display the chance of rain for tomorrow
-    print(f"Chance of rain for tomorrow: {chance_of_rain}%") #think this just adds up the total percentages.
 
 
-else:
-    # Handle the error
-    print(f"Error: API request failed with status code {response.status_code}")
+    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}', params = parameters)
+
+    #print(response.text)
+    if response.status_code == 200:
+        # The request was successful (HTTP 200 OK).
+        
+        # Parse the JSON response
+        data = response.json()
+        
+        # Put data into dictionary
+        data_dict = {
+            'time' : data['current']['time'],
+            'day_of_the_week' : data['current']['is_day'] + 1,
+            'temperature' : data['current']['temp_f'],
+            #'description' : data['current']['condition']['text'],
+            'temp_feel' : data['current']['feelslike_f'],
+           # 'weather_code' : data['current']['code'],
+            'wind_mph' : data['current']['wind_mph'],
+            'wind_degree' : data['current']['wind_degree'],
+            'pressure_mb' : data['current']['pressure_mb'],
+            'precipitation_mm' : data['current']['precip_mm'],
+            'humidity' : data['current']['humidity'],
+            'cloudiness' : data['current']['cloud'],
+            'uv_index' : data['current']['uv'],
+            'gust_mph' : data['current']['gust_mph'],
+        }
+
+        # Access and use the data
+        temperature = data['current']['temp_f']
+        description = data['current']['condition']['text']
+        day_of_the_week = data['current']['is_day'] + 1
+        temp_feel = data['current']['feelslike_f']
+       # weather_code = data['current']['code']
+        # Get today's date and calculate the date for the next day
+        today = datetime.now().date()
+        next_day = today + timedelta(days=1)
+
+        # Find the forecast for the next day
+        '''for forecast in data['forecast']['forecastday']:
+            date = datetime.strptime(forecast['date'], '%Y-%m-%d').date()
+            if date == next_day:
+                next_day_forecast = forecast
+                break
+        '''
+        #chance_of_rain = tomorrow_forecast['day']['daily_chance_of_rain']
+        # Print the weather information
+        '''   print("Date:", today)
+        print(f"Day of the Week: {day_of_the_week}")
+        print(f"Temperature: {temperature}°F")
+        print(f"Feels Like: ", temp_feel, "°F")
+        print(f"Weather Code: ", weather_code)
+        print(f"Description: {description}")
+        print()
+        # Print forcast for next day
+        print("Forecast for the next day:")
+        print(f"Date: {next_day_forecast['date']}")
+    #  print(f"Day of the Week: {next_day_forecast['is_day']}")
+        print(f"Temperature: {next_day_forecast['day']['avgtemp_f']}°F")
+        #print(f"Description: {next_day_forecast['day']['condition']['text']}") this prints text, not a percentage of probability it will rain
+        print(f"Description: {next_day_forecast['day']['daily_chance_of_rain']}") #this just adds all the percentages together
+        print("other alg")
+        '''
+        '''
+        tomorrow = today + timedelta(days=1)
+        for forecast in data['forecast']['forecastday']:
+            date = datetime.strptime(forecast['date'], '%Y-%m-%d').date()
+            if date == tomorrow:
+                tomorrow_forecast = forecast
+                break
+            
+        # Extract the chance of rain for tomorrow
+        chance_of_rain = tomorrow_forecast['day']['daily_chance_of_rain']
+
+        # Display the chance of rain for tomorrow
+        print(f"Chance of rain for tomorrow: {chance_of_rain}%") #think this just adds up the total percentages.
+        '''
+
+    else:
+        # Handle the error
+        print(f"Error: API request failed with status code {response.status_code}")
+
+    return data_dict
+
+print(get_dict())
+
+'''def get_dict():
+    return data_dict'''
