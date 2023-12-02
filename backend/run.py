008bebdcc044202dea79bb35162358625bb1efa0
Ethan Ikegami <ikeg225@berkeley.edu>
2023-12-01 16:42:58 -0800

    backend ready

--- backend/run.py
+++ backend/run.py
@@ -6,10 +6,8 @@ import pickle
 app = Flask(__name__)
 api = Api(app)
 
-# Load pipeline and model using the binary files
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Function to test if the request contains multiple 
 def islist(obj):
   return True if ("list" in str(type(obj))) else False
 
@@ -17,25 +15,18 @@ class Preds(Resource):
   def put(self):
     json_ = request.json
     entry = pd.DataFrame([json_])
-    # Transform request data record/s using the pipeline
-    # entry_transformed = pipeline.transform(entry)
-    # Make predictions using transformed data
-    print(json_)
     prediction = model.predict(entry)
-    res = {'predictions': {}}
-
-    print(prediction)
-    # # Create the response
-    # for i in range(len(prediction)):
-    #   res['predictions'][i + 1] = int(prediction[i])
-
-    return res, 200 # Send the response object
+    res = {'prediction': prediction[0]}
+    return res, 200
 
 api.add_resource(Preds, '/predict')
 
 if __name__ == "__main__":
   app.run(debug = True)
 
-# to run the flask app
+# to run the flask app (make sure to install Flask)
 # export FLASK_APP=run
 # flask run
+
+# example request
+# curl -XPUT -H "Content-type: application/json" -d '{"day_of_week": 5, "temperature": 47, "temp_feel": 41, "weather_code": 1189, "wind_mph": 23.7, "wind_degree": 178, "pressure_mb": 1015, "precipitation_mm": 0, "humidity": 90, "cloudiness": 50, "uv_index": 1, "gust_mph": 13.3, "school_break": 0, "is_holiday": 0, "is_rrr_week": 0, "is_finals_week": 0, "is_student_event": 0, "hour": 18}' 'http://127.0.0.1:5000/predict'
